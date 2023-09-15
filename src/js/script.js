40f773ba1eeef031febe1d66fe85b9afa8be2828
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2023-09-15 21:46:48 +1000

    shapes functions and finish all tests

--- src/js/script.js
+++ src/js/script.js
@@ -1,45 +1,12 @@
-import morseCodeMap from "./data.js";
+import { TextToMorseCode, MorseCodeToText } from "./functions.js";
 
 const textInput = document.getElementById("text");
 const morseCodeInput = document.getElementById("morse");
 
-export const TextToMorseCode = (e) => {
-  if (e.target.value.match(/[^A-Za-z0-9\s]/)) {
-    morseCodeInput.value = "";
-    throw new Error("Your text can only contain letters or numbers");
-  }
+textInput.addEventListener("input", (e) => {
+  morseCodeInput.value = TextToMorseCode(e.target.value);
+});
 
-  const morseCodeArray = e.target.value
-    .split("")
-    .map((char) => morseCodeMap[char.toUpperCase()] + " ");
-
-  morseCodeInput.value = morseCodeArray.join("");
-  return morseCodeArray.join("");
-};
-
-export const MorseCodeToText = (e) => {
-  if (e.target.value.match(/[^.\-\/\s]/)) {
-    textInput.value = "";
-    throw new Error("Your morse code can only contain '.', '-' or '/'");
-  }
-  const typedMorseCode = e.target.value.split(" ");
-  typedMorseCode.includes("") ? typedMorseCode.pop() : typedMorseCode;
-
-  let text = [];
-  for (const block of typedMorseCode) {
-    for (const key in morseCodeMap) {
-      if (morseCodeMap[key] === block) {
-        text.push(key.toLowerCase());
-        break;
-      } else if (!Object.values(morseCodeMap).includes(block)) {
-        textInput.value = "";
-        throw new Error("Does not correspond to a letter, word or number");
-      }
-    }
-  }
-  textInput.value = text.join("");
-  return text.join("");
-};
-
-textInput.addEventListener("input", TextToMorseCode);
-morseCodeInput.addEventListener("input", MorseCodeToText);
+morseCodeInput.addEventListener("input", (e) => {
+  textInput.value = MorseCodeToText(e.target.value);
+});
---
+++ tests/functions.test.js
@@ -0,0 +1,104 @@
+import { TextToMorseCode, MorseCodeToText } from "../src/js/functions.js";
+
+describe("TextToMorseCode() tests", () => {
+  it("Should throw an error if non-letter/number characters are typed into the input", () => {
+    expect(() => {
+      TextToMorseCode("hello!");
+    }).toThrow(new Error("Your text can only contain letters or numbers"));
+    expect(() => {
+      TextToMorseCode("hello, are you ok?");
+    }).toThrow(new Error("Your text can only contain letters or numbers"));
+    expect(() => {
+      TextToMorseCode("S.O.S.");
+    }).toThrow(new Error("Your text can only contain letters or numbers"));
+    expect(() => {
+      TextToMorseCode("!@#$%^");
+    }).toThrow(new Error("Your text can only contain letters or numbers"));
+  });
+
+  it("Should be able to handle uppercase and lowercase characters", () => {
+    expect(TextToMorseCode("hello")).toBe(".... . .-.. .-.. --- ");
+    expect(TextToMorseCode("hElLo")).toBe(".... . .-.. .-.. --- ");
+    expect(TextToMorseCode("HEllo")).toBe(".... . .-.. .-.. --- ");
+    expect(TextToMorseCode("HELLO")).toBe(".... . .-.. .-.. --- ");
+  });
+
+  it("Should transform any text string into morse code", () => {
+    expect(TextToMorseCode("apple")).toBe(".- .--. .--. .-.. . ");
+    expect(TextToMorseCode("hello world")).toBe(
+      ".... . .-.. .-.. --- / .-- --- .-. .-.. -.. "
+    );
+    expect(
+      TextToMorseCode("there are over 7 billion people in the world")
+    ).toBe(
+      "- .... . .-. . / .- .-. . / --- ...- . .-. / --... / -... .. .-.. .-.. .. --- -. / .--. . --- .--. .-.. . / .. -. / - .... . / .-- --- .-. .-.. -.. "
+    );
+    expect(TextToMorseCode("1997")).toBe(".---- ----. ----. --... ");
+  });
+});
+
+describe("MorseCodeToText() tests", () => {
+  it("Should throw an error if any group of characters contain other types beyond the following: '.', '-' or '/' ", () => {
+    expect(() => {
+      MorseCodeToText(".... . .-.. .-.. --_");
+    }).toThrow(new Error("Your morse code can only contain '.', '-' or '/'"));
+    expect(() => {
+      MorseCodeToText(".... . ._.. ._.. ___");
+    }).toThrow(new Error("Your morse code can only contain '.', '-' or '/'"));
+    expect(() => {
+      MorseCodeToText(".... , .-.. .-.. ---");
+    }).toThrow(new Error("Your morse code can only contain '.', '-' or '/'"));
+    expect(() => {
+      MorseCodeToText("hello");
+    }).toThrow(new Error("Your morse code can only contain '.', '-' or '/'"));
+  });
+
+  it("Should throw an error if a block of morse code characters does not correspond a letter or number", () => {
+    expect(() => {
+      MorseCodeToText("......");
+    }).toThrow(new Error("The space between characters is irregular"));
+    expect(() => {
+      MorseCodeToText("./");
+    }).toThrow(new Error("The space between characters is irregular"));
+    expect(() => {
+      MorseCodeToText("./.");
+    }).toThrow(new Error("The space between characters is irregular"));
+    expect(() => {
+      MorseCodeToText("..  ..");
+    }).toThrow(new Error("The space between characters is irregular"));
+  });
+
+  it("Should handle spaces between characters with a ' ' separator", () => {
+    expect(MorseCodeToText(".")).toBe("e");
+    expect(MorseCodeToText("..")).toBe("i");
+    expect(MorseCodeToText(". .")).toBe("ee");
+    expect(MorseCodeToText("...")).toBe("s");
+    expect(MorseCodeToText(".. .")).toBe("ie");
+  });
+
+  it("Should handle spaces between words with a '/' separator", () => {
+    expect(MorseCodeToText("..")).toBe("i");
+    expect(MorseCodeToText(". / .")).toBe("e e");
+    expect(MorseCodeToText(". / ..")).toBe("e i");
+    expect(MorseCodeToText(".. / .")).toBe("i e");
+    expect(MorseCodeToText(".... . .-.. .-.. --- / -.-- --- ..-")).toBe(
+      "hello you"
+    );
+    expect(MorseCodeToText(".... . .-.. .-.. --- -.-- --- ..-")).toBe(
+      "helloyou"
+    );
+  });
+
+  it("Should transform any morse code string into text", () => {
+    expect(MorseCodeToText("-.-- . .-.. .-.. --- .--")).toBe("yellow");
+    expect(
+      MorseCodeToText(
+        "...-- / ..--- / .---- / .... . .-. . / .. / -.-. --- -- ."
+      )
+    ).toBe("3 2 1 here i come");
+    expect(MorseCodeToText(".-. .- -. -.. --- -- / .-- --- .-. -.. ...")).toBe(
+      "random words"
+    );
+    expect(MorseCodeToText("..--- ----- ..--- ...--")).toBe("2023");
+  });
+});
